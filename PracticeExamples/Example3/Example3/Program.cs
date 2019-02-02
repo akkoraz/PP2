7375c097b7b475d34c3a179b90529a4e736054a2
akkoraz <47221478+akkoraz@users.noreply.github.com>
2019-02-02 10:47:20 +0600

    Week0

---
+++ PracticeExamples/Example3/Example3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = float.Parse(Console.ReadLine());
+            //double y = double.Parse(Console.ReadLine());
+            //string z= Console.ReadLine();
+            bool isTrue = false;
+            if (x > 4)
+            {
+                isTrue = true;
+            }
+            if (isTrue) Console.WriteLine("Number " + x + " more than 4");
+            Console.ReadKey();
+        }
+    }
+}
